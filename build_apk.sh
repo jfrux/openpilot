f1fbddf695dda28b2c309045db24ce5f8d030598
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-07-18 21:32:04 -0400

    test

--- build_apk.sh
+++ build_apk.sh
@@ -1,3 +1,3 @@
-apktool b ../ai.comma.plus.frame/   -o ./apk/ai.comma.plus.frame.apk
-apktool b ../ai.comma.plus.offroad/ -o ./apk/ai.comma.plus.offroad.apk
-apktool b ../ai.comma.plus.black/   -o ./apk/ai.comma.plus.black.apk
+apktool b ../ai.comma.plus.frame/   -o ./apk/ai.comma.plus.frame.apk -c
+apktool b ../ai.comma.plus.offroad/ -o ./apk/ai.comma.plus.offroad.apk -c
+apktool b ../ai.comma.plus.black/   -o ./apk/ai.comma.plus.black.apk -c
